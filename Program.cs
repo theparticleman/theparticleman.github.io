78d99629bb27eebba09c21cf90d8867c81849c2b
Jon Turner <theparticleman@gmail.com>
2021-05-22 21:49:56 -0600

    Initial start of blog

--- Program.cs
+++ Program.cs
@@ -7,15 +7,25 @@ namespace MythicantSite
     class Program
     {
         const string IndexPath = "docs/index.html";
+        const string MainBlogPath = "docs/blog.html";
+        static string template;
 
         static void Main(string[] args)
         {
+            template = File.ReadAllText("template");
+
             GenerateIndexPage();
+            GenerateMainBlogPage();
+        }
+
+        private static void GenerateMainBlogPage()
+        {
+            var blogHtml = template.Replace("{template}", "");
+            File.WriteAllText(MainBlogPath, blogHtml);
         }
 
         private static void GenerateIndexPage()
         {
-            var template = File.ReadAllText("template");
             var gamesHtml = string.Join("\r\n", Games.List.Select(x => x.ToHtml()));
             gamesHtml = $"<div class='games-list'>\r\n{gamesHtml}\r\n</div>";
             var indexHtml = template.Replace("{template}", gamesHtml);
